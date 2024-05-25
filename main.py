8aa8247c439cd9b716347e23d9efb61a50903920
Davronov Otajon <132939514+Otajon2300@users.noreply.github.com>
2024-05-25 07:08:34 +0500

    bot

---
+++ main.py
@@ -0,0 +1,40 @@
+import logging
+from background import keep_alive
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Salom! Men guruhda qo\'shilish va chiqish xabarlarini yashiraman. \nMeni guruhga qo\'shib admin sifatida tayinlang. ')
+
+async def handle_new_chat_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    for member in update.message.new_chat_members:
+        try:
+            await context.bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
+            logger.info(f"{member.full_name} uchun qo'shilish xabarini o'chirdim {update.message.chat.title}")
+        except Exception as e:
+            logger.error(f"Qo'shilish xabarini o'chirishda xato: {e}")
+
+async def handle_left_chat_member(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    try:
+        await context.bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
+        logger.info(f"Chiqish xabarini o'chirdim {update.message.chat.title}")
+    except Exception as e:
+        logger.error(f"Chiqish xabarini o'chirishda xato: {e}")
+
+keep_alive()
+if __name__ == '__main__'
+
+    # Botingizning API tokenini kiriting
+    # token = '6328093327:AAGBnOnAzeVK-5Tkk9DPqYe90Tu_ccL4wZg'
+    token=os.environ.get('TOKEN')
+
+    app = ApplicationBuilder().token(token).build()
+
+    # Handlerlarni ro'yxatga olish
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, handle_new_chat_members))
+    app.add_handler(MessageHandler(filters.StatusUpdate.LEFT_CHAT_MEMBER, handle_left_chat_member))
+
+    # Botni ishga tushirish
+    app.run_polling()
